5d86be6d30b00a172248cc7631eb4f2995001f7b
Nicolas Ganz <nicolas@keltec.ch>
2017-10-05 23:21:57 +0200

    added test for --version

--- spec/features/dirwatch_spec.rb
+++ spec/features/dirwatch_spec.rb
@@ -33,4 +33,13 @@ Allowed optional arguments: 1
 EOT
     end
   end
+
+  context 'prints a version' do
+    it 'with --version' do
+      expect { run '--version' }.to output(/\Adirwatch \d+\.\d+\.\d+\n\z/).to_stdout
+        .and not_output.to_stderr
+      expect { run '--version' }.to output("dirwatch #{Dirwatch::VERSION}\n").to_stdout
+        .and not_output.to_stderr
+    end
+  end
 end
